2ae4dd58753fbd1204d8bab53512f51afe3900a5
Heero.Law <heeroluo@gmail.com>
2017-11-27 16:42:00 +0800

    ES6化

--- bin/index.js
+++ bin/index.js
@@ -1,17 +1,17 @@
 #!/usr/bin/env node
 
 /*!
- * Command line tools for Back2Front
+ * Command line interface tools for Back2Front
  * 命令入口
  */
 
 'use strict';
 
-var path = require('path'),
-	fs = require('fs'),
-	argvs = require('minimist')(process.argv.slice(2)),
-	jsonFormat = require('json-format'),
-	errorExit = require('../lib/util').errorExit;
+const path = require('path');
+const fs = require('fs');
+const jsonFormat = require('json-format');
+const errorExit = require('../lib/util').errorExit;
+const argvs = require('minimist')(process.argv.slice(2));
 
 
 // 带参数v时，仅打印版本号
@@ -21,7 +21,7 @@ if (argvs.v === true) {
 }
 
 
-var fn;
+let fn;
 switch (argvs._[0]) {
 	// 构建
 	case 'build':
@@ -39,10 +39,10 @@ switch (argvs._[0]) {
 
 
 // 检查项目路径是否存在
-var pjPath = argvs._[1];
+let pjPath = argvs._[1];
 if (pjPath) {
 	pjPath = path.resolve(pjPath);
-	if ( !fs.existsSync(pjPath) ) {
+	if (!fs.existsSync(pjPath)) {
 		errorExit('"' + pjPath + '" does not exist');
 	}
 } else {
@@ -50,18 +50,18 @@ if (pjPath) {
 }
 
 // 检查构建配置文件是否存在
-var configPath = path.join(pjPath, 'build-config.json');
-if ( !fs.existsSync(configPath) ) {
-	errorExit('Configuration file not found.');
+const configPath = path.join(pjPath, 'build-config.json');
+if (!fs.existsSync(configPath)) {
+	errorExit('Configuration file not found');
 }
 
 
-var result = fn(pjPath, argvs, require(configPath));
+const result = fn(pjPath, argvs, require(configPath));
 
 // 输出依赖分析结果
 if (argvs._[0] === 'depa') {
 	result.then(function(map) {
-		// 带o参数时，则输出到指定路径，否则直接打印
+		// 带o参数时，输出到指定路径，否则直接打印
 		if (argvs.o) {
 			fs.writeFileSync(
 				path.resolve(argvs.o),
